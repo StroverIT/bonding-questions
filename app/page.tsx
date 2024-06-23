f42e4bb32da9d536e6352135a27e56735e9526a5
StroverIT <xcreaw1@abv.bg>
2024-06-24 00:31:26 +0300

    Changing to getting random question and setting it

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,15 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { getRandomQuestion } from "@/utils/questions";
 
 export default function Home() {
-  const [question, setQuestion] = useState(null);
+  const [question, setQuestion] = useState<string | null>(null);
 
-  const generateQuestionHandler = async () => {
-    const response = await fetch("/api/chat-gpt", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log("data", data);
-    setQuestion(data?.message.choices[0].message.content);
+  const setRandomQuestion = () => {
+    setQuestion(getRandomQuestion());
   };
+
   return (
     <>
       <div className="h-screen w-screen relative z-10">
@@ -28,7 +22,7 @@ export default function Home() {
         />
       </div>
       <div className="fixed h-screen w-screen bg-black opacity-30 z-20 top-0 left-0" />
-      <main className="fixed top-0 left-0 h-full container z-30 text-white ">
+      <main className="fixed top-0 left-0 h-full  z-30 text-white w-screen">
         <div className="h-full flex items-center justify-center flex-col">
           <h1 className="text-4xl font-bold mb-10 text-center">
             Welcome to the Bonding questions
@@ -37,14 +31,14 @@ export default function Home() {
           <div className="flex flex-col items-center justify-center space-y-4">
             <button
               className="p-2 bg-blue-500 text-white rounded"
-              onClick={generateQuestionHandler}
+              onClick={setRandomQuestion}
             >
               Generate a question
             </button>
           </div>
-          {true && (
+          {question && (
             <div className="mt-10 text-red-600 font-bold bg-[#ffebe9] p-10 rounded-xl">
-              BLa bla{question}
+              {question}
             </div>
           )}
         </div>
