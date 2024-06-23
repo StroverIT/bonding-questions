04975dc81096099c4a96b2c523817c7794e8429b
StroverIT <xcreaw1@abv.bg>
2024-06-24 00:31:46 +0300

    getting random question based on questions length

---
+++ utils/questions.js
@@ -0,0 +1,6 @@
+import { questions } from "@/data/questions";
+
+export const getRandomQuestion = () => {
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+};
